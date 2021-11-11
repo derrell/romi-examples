752025717f58da5ac53fd633cc6961e9d7361de0
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2021-11-11 10:29:01 -0500

    Allow running the motor based on joystick input Y

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,28 +1,35 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.Spark;
 
 public class Robot extends TimedRobot
 {
   private Spark leftMotor;
+  private XboxController driverController;
+
   
   /**
    * This function is called when the robot is first started up and
    * should be used for any initialization code.
    */
   @Override
-  public void teleopInit()
+  public void robotInit()
   {
     // The Romi has the left and right motors set to
     // PWM channels 0 and 1 respectively
     leftMotor = new Spark(0); // Spark is a type of motor controller
+
+    // Connect to the XBox controller
+    driverController = new XboxController(0);
   }
 
   /** This function is called periodically (every 20ms) */
   @Override
   public void teleopPeriodic()
   {
-    leftMotor.set(0.5);
+    // Control left motor with Joystick 0, Axis 5
+    leftMotor.set(-driverController.getY());
   }
 }
